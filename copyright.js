f6e92b6c413824821f5f0f6bbc7cb9b96de8793f
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-25 14:47:55 -0700

    Add files via upload

--- copyright.js
+++ copyright.js
@@ -3,6 +3,6 @@ document.write("Copyright ");
 var alink = document.createElement("a");
 alink.href = "http://www.jointheleague.org";
 alink.text = "League of Amazing Programmers";
-document.getElementsByTagName("body")[0].appendChild(alink)
+document.getElementsByTagName("body")[0].appendChild(alink);
 
 document.write(" 2013-1017");
