22a458f423d4cf5b1eb8c0781b5fdd66416a15e3
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-26 14:16:11 -0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 function addFooter() {
 var footer;
 footer = document.getElementById("moduleFooter");
-footer.innerHTML += "<a href=\"https://www.jointheleague.org\"><img src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\"></a>";
-footer.innerHTML +=  "<a href=\"http://jointheleague.github.io\"><img src=\"https://league-level0.github.io/teacher.png\" alt=\"Teacher Info\"></a>";
-fotter.innerHTML += "<a href=\"https://jtl.pike13.com\"><img src=\"https://league-level0.github.io/pike13.png\" alt=\"Legaue Hub\"></a>";
+footer.innerHTML += "<a href=\"https://www.jointheleague.org\"><img src=\"https://league-level0.github.io/league.jpg\" style="width: 33%;" alt=\"league-logo\"></a>";
+footer.innerHTML +=  "<a href=\"http://jointheleague.github.io\"><img src=\"https://league-level0.github.io/teacher.png\" style="width: 33%;" alt=\"Teacher Info\"></a>";
+fotter.innerHTML += "<a href=\"https://jtl.pike13.com\"><img src=\"https://league-level0.github.io/pike13.png\" style="width: 33%;" alt=\"Legaue Hub\"></a>";
 }
