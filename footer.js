92d8a0a7c08bdc23f872d60d94a87d9118bdc319
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-26 13:19:36 -0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,4 +2,6 @@ function addFooter() {
 var footer;
 footer = document.getElementById("moduleFooter");
 footer.innerHTML += "<a href=\"https://www.jointheleague.org\"><img src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\"></a>"
+footer.innerHTML +=  "<a href=\"http://jointheleague.github.io\"> Teacher Support </a>";
+fotter.innerHTML += "<a href=\"https:jtl.pike13.com"> League Hub  </a>";
 }
