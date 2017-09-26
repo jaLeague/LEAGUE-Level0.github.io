2db8db1b34b66192078e8138bfabde5d7da80b81
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-26 12:27:13 -0700

    footer.js

---
+++ footer.js
@@ -0,0 +1,4 @@
+
+var footer;
+footer = document.getElementsByTagName("moduleFooter");
+footer.innerHTML = "<a href=\"https://www.jointheleague.org/\"><img src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\"></a>";
