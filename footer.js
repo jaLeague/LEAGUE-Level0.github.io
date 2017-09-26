0c479b8c021bf878bcc93f0fbbb3dcda6ab622c4
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-26 13:14:13 -0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 function addFooter() {
 var footer;
 footer = document.getElementById("moduleFooter");
-footer.innerHTML += "<p>addFooter function</p>";
+footer.innerHTML += "<a href=\"https://www.jointheleague.org\"><img src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\"></a>"
 }
