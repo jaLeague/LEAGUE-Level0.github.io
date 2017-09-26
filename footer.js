41ce1d0b97b754f07c40d6020b8c5b6294a5df81
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-26 15:30:58 -0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -4,7 +4,7 @@ footer = document.getElementById("moduleFooter");
 
 footer.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\" align=\"left\" style=\"float:left\"></a>";
  
-  footer.innerHTML +=  "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/teacher.png\" alt=\"Teacher Info\"  align=\"center\" ></a>";
+  footer.innerHTML +=  "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/teacher.gif\" alt=\"Teacher Info\"  align=\"center\" ></a>";
 
   footer.innerHTML += "<a href=\"https://jtl.pike13.com\"><img class=\"footer\" src=\"https://league-level0.github.io/pike13.png\" alt=\"Legaue Hub\" align=\"right\" style=\"float:right\"></a>";
 
