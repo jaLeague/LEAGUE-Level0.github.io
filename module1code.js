db692da0084fb592698b2f776284ed9f36750e63
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-27 14:13:53 -0700

    Create module1code.js

---
+++ module1code.js
@@ -0,0 +1,8 @@
+function module1code() {
+var starter;
+
+starter = document.getElementById("starterCode");
+
+starter.innerHTML += "<a href=\"http://bit.ly/l0m1test\">Get module 1 Java project</a>";
+  
+}
