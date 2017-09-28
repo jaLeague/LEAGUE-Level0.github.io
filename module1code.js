553d24d81acb0d498826f021d23ed6035f02fd64
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-28 11:57:29 -0700

    Update module1code.js

--- module1code.js
+++ module1code.js
@@ -1,7 +1,7 @@
 function module1code() {
 var starter;
 
-starter = document.getElementById("starterCode");
+starter = document.getElementById("moduleLink");
 
 starter.innerHTML += "<a href=\"http://bit.ly/l0m1test\">Get module 1 Java project</a>";
   
