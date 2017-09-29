1fcf4ba1cfce2ef6d4889f7bdac317845394937b
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-29 12:19:36 -0700

    Update teacherHeader.js

--- teacherHeader.js
+++ teacherHeader.js
@@ -1,15 +1,13 @@
 function teacherHeader() {
-var left;
- var center;
- var right;
-left = document.getElementById("leftHead");
-center = document.getElementById("centerHead");
- right = document.getElementById("rightHead");
- 
-left.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\" align=\"center\"></a>";
- 
-center.innerHTML +=  "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/teacher.png\" alt=\"Teacher Resources\"  align=\"center\"</a>";
+var links;
 
-right.innerHTML += "<a href=\"https://jtl.pike13.com\"><img class=\"footer\" src=\"https://league-level0.github.io/pike13.png\" alt=\"Legaue Hub\"  align=\"center\"></a>";
+links = document.getElementById("teacherLinks");
 
+links.innerHTML += "<table><tr><td>";
+links.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\" align=\"center\"></a>";
+links.innerHTML += "</td><td>";
+links.innerHTML += "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/teacher.png\" alt=\"Teacher Resources\"  align=\"center\"</a>";
+links.innerHTML += "</td><td>";
+ links.innerHTML += "<a href=\"https://jtl.pike13.com\"><img class=\"footer\" src=\"https://league-level0.github.io/pike13.png\" alt=\"Legaue Hub\"  align=\"center\"></a>";
+links.innerHTML += "</td></tr></table>";
 }
