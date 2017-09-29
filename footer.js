0849e584403e0f5feb986ecc124732558e60c772
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-29 11:37:14 -0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -8,7 +8,7 @@ center = document.getElementById("centerDiv");
  
 left.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\" align=\"center\"></a>";
  
-center.innerHTML +=  "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/teacher.gif\" alt=\"Teacher Info\"  align=\"center\"</a>";
+center.innerHTML +=  "<a href=\"https://docs.oracle.com/javase/8/docs/api/\"><img class=\"footer\" src=\"https://league-level0.github.io/java.png\" alt=\"Java API\"  align=\"center\"</a>";
 
 right.innerHTML += "<a href=\"https://processing.org/reference\"><img class=\"footer\" src=\"https://league-level0.github.io/p3logo.jpeg\" alt=\"Processing Reference\"  align=\"center\"></a>";
 
