39f7f00661326c528b72e75ba3f76f5a6e81f877
jaLeague <jackie.arseneau@jointheleague.org>
2017-09-29 12:38:55 -0700

    Update teacherHeader.js

--- teacherHeader.js
+++ teacherHeader.js
@@ -3,11 +3,11 @@ var links;
 
 links = document.getElementById("teacherLinks");
 
-links.innerHTML += "<table><tr><td>";
+links.innerHTML += "<span>";
 links.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\"></a>";
-links.innerHTML += "</td><td>";
+links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
 links.innerHTML += "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/teacher.png\" alt=\"Teacher Resources\"</a>";
-links.innerHTML += "</td><td>";
+links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
 links.innerHTML += "<a href=\"https://jtl.pike13.com\"><img class=\"footer\" src=\"https://league-level0.github.io/pike13.png\" alt=\"Legaue Hub\"></a>";
-links.innerHTML += "</td></tr></table>";
+links.innerHTML += "</span>";
 }
