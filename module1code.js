e0d60b50b0377771e1d56b6920302546bc21379a
jaLeague <jackie@WTS19.attlocal.net>
2017-09-29 15:28:26 -0700

    h3

--- module1code.js
+++ module1code.js
@@ -3,6 +3,6 @@ var starter;
 
 starter = document.getElementById("moduleLink");
 
-starter.innerHTML += "<h2><a href=\"http://bit.ly/l0m1test\">Get module 1 Java project</a></h2>";
+starter.innerHTML += "<h3><a href=\"http://bit.ly/l0m1test\">Get module 1 Java project</a></h3>";
   
 }
