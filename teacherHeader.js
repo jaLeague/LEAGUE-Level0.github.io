237c69a4f947c34659da16150e8a76e612028772
jaLeague <jackie.arseneau@jointheleague.org>
2017-10-19 13:10:49 -0700

    Add recipe suggestion box

--- teacherHeader.js
+++ teacherHeader.js
@@ -9,5 +9,6 @@ links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
 links.innerHTML += "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/teacher.png\" alt=\"Teacher Resources\"</a>";
 links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
 links.innerHTML += "<a href=\"https://jtl.pike13.com\"><img class=\"footer\" src=\"https://league-level0.github.io/pike13.png\" alt=\"Legaue Hub\"></a>";
+links.innerHTML += "<a href=\"mailto:curriculum@jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/suggestions.jpg\" alt=\"Recipe Suggestions\"</a>";  
 links.innerHTML += "</span>";
 }
