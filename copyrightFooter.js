1d137eb5fa88f12f0e0a5e85b18d6f21ff3803a5
jaLeague <jackie.arseneau@jointheleague.org>
2018-05-01 13:50:12 -0700

    Update copyrightFooter.js

--- copyrightFooter.js
+++ copyrightFooter.js
@@ -1,5 +1,5 @@
 function copyright() {
 var footer;
 footer = document.getElementById("copyright");
-footer.innerHTML += "Copyright <a href=\"http://www.jointheleague.org\">League of Amazing Programmers</a> 2013-2017";
+footer.innerHTML += "Copyright <a href=\"http://www.jointheleague.org\">League of Amazing Programmers</a> 2013-2018";
 }
