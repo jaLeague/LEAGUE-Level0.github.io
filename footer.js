a2e4f45fb83a9c686f2203f3016a9ead170c773d
jaLeague <jackie.arseneau@jointheleague.org>
2018-05-01 13:42:42 -0700

    Update footer.js

--- footer.js
+++ footer.js
@@ -4,12 +4,14 @@ var left;
  var right;
 left = document.getElementById("leftDiv");
 center = document.getElementById("centerDiv");
- right = document.getElementById("rightDiv");
+right = document.getElementById("rightDiv");
  
-left.innerHTML += "<a href=\"http://www.jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\" align=\"center\"></a>";
+left.innerHTML += "<a href=\"http://www.jointheleague.org\">
+    <img class=\"footer\" src=\"https://league-level0.github.io/league.jpg\" alt=\"league-logo\" align=\"center\" height: 225.00px;></a>";
  
-center.innerHTML +=  "<a href=\"https://docs.oracle.com/javase/8/docs/api/\"><img class=\"footer\" src=\"https://league-level0.github.io/java.png\" alt=\"Java API\"  align=\"center\"</a>";
+center.innerHTML +=  "<a href=\"https://docs.oracle.com/javase/8/docs/api/\">
+    <img class=\"footer\" src=\"https://league-level0.github.io/java.png\" alt=\"Java API\"  align=\"center\" height: 225.00px;></a>";
 
-right.innerHTML += "<a href=\"https://processing.org/reference\"><img class=\"footer\" src=\"https://league-level0.github.io/p3logo.jpeg\" alt=\"Processing Reference\"  align=\"center\"></a>";
+right.innerHTML += "<a href=\"https://processing.org/reference\"><img class=\"footer\" src=\"https://league-level0.github.io/p3logo.jpeg\" alt=\"Processing Reference\"  align=\"center\" height: 225.00px;></a>";
 
 }
