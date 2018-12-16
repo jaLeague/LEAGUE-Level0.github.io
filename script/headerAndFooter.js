6ce62110bc0aba2cccef1b9f780555e3c1dde510
mjfre <theleague@Matts-MBP.attlocal.net>
2018-12-16 10:39:01 -0800

    New image icons for top bar
    Probably will be edited or removed depending on how they look

--- script/headerAndFooter.js
+++ script/headerAndFooter.js
@@ -9,7 +9,7 @@ function addHeader() {
     links.innerHTML += "<a href=\"http://jointheleague.github.io\"><img class=\"footer\" src=\"https://league-level0.github.io/img/teacher.png\" alt=\"Teacher Resources\"</a>";
     links.innerHTML += "</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>";
     links.innerHTML += "<a href=\"https://jtl.pike13.com\"><img class=\"footer\" src=\"https://league-level0.github.io/img/pike13.png\" alt=\"Legaue Hub\"></a>";
-    links.innerHTML += "<a href=\"mailto:curriculum@jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/img/suggestions.jpg\" alt=\"Recipe Suggestions\"</a>";  
+    links.innerHTML += "<a href=\"mailto:curriculum@jointheleague.org\"><img class=\"footer\" src=\"https://league-level0.github.io/img/suggestions.png\" alt=\"Recipe Suggestions\"</a>";  
     links.innerHTML += "</span>";
 }
 
