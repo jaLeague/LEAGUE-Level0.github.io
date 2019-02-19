ba03790dcc900deacbe57fc010ed9395bc26d72e
mjfre <theleague@Matts-MBP.attlocal.net>
2019-02-18 17:12:33 -0800

    Updated Module 4 recipe style

--- script/recipeGenerator.js
+++ script/recipeGenerator.js
@@ -18,12 +18,7 @@ function createRecipeHTML2(recipeTitle, recipeGoal, recipeSteps){
     return preformatted;
 }
 
-
-
-
-
-
-function createRecipeHTML(recipeTitle, recipeGoal, recipeSteps){
+function createRecipeHTML(recipeTitle, containsTitleImage, titleImageFileName, recipeGoal, recipeSteps){
                 var htmlTags = '<!DOCTYPE html>'
                                 +'<html>';
 
@@ -32,10 +27,10 @@ function createRecipeHTML(recipeTitle, recipeGoal, recipeSteps){
                         +'<title> ' + recipeTitle + ' - League Level 0</title>'
                         +'<meta charset="UTF-8">'
                         +'<meta name="viewport" content="width=device-width, initial-scale=1">'
-                        +'<link rel="stylesheet" href="http://level0.jointheleague.org/style/style.css">'
+                        +'<link rel="stylesheet" href="https://league-central.github.io/curriculum/style/style.css">'
                         +'<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">'
-                        +'<link rel="icon" type="image/png" href="../../img/favicon.png">'
-                        +'<script src="http://level0.jointheleague.org/script/headerAndFooter.js"></script>'
+                        +'<link rel="icon" type="image/png" href="https://league-central.github.io/curriculum/img/favicon.png">'
+                        +'<script src="https://league-central.github.io/curriculum/script/headerAndFooter.js"></script>'
                     +'</head>';
     
     
@@ -49,9 +44,14 @@ function createRecipeHTML(recipeTitle, recipeGoal, recipeSteps){
                                 +'<div id = "recipePage">'
                                     +'<div id="recipeTitle">'
                                         +'<h1>'+recipeTitle+'</h1>'                                        
-                                        +'<hr>'
-                                        +'<img src="images/googlyEyes.png" alt="'+recipeTitle+'image">'
-                                    +'</div>'
+                                        +'<hr>';
+    
+                if(containsTitleImage){
+                    body+= '<img src='+titleImageFileName+' alt="'+recipeTitle+' image">'
+                }
+                                      
+                body+=
+                                    '</div>'
                                     +'<div id ="recipeBody">'
                                         +'<div id="recipeGoal">'
                                             +'<h2>Goal:</h2>'
@@ -83,31 +83,13 @@ function createRecipeHTML(recipeTitle, recipeGoal, recipeSteps){
     }
 
 function formatRecipeSteps(recipeStepsString){
-    var steps = recipeStepsString;
-    var regex = /[1-9][0-9]?\.\s/;
-    var output = ""
-    //alert(steps.substring(24));
-    //alert(steps);
-    while(steps.search(regex) >= 0){
-        var indexStart = steps.search(regex);
-        var indexEnd = steps.slice(indexStart+4).search(regex);
-        //alert("slice: \n" + steps.slice(indexStart+4));
-        if(indexEnd===-1){
-            indexEnd = steps.length;
-        }
-        //alert(indexStart + " " + indexEnd + " " + steps.substring(indexStart, indexEnd));
-        output+='<li>' + steps.substring(indexStart, indexEnd).trim() + '</li>';
-        steps = steps.substring(indexEnd);
+    recipeStepsString = recipeStepsString.trim();
+    recipeStepsString+="\n9.";   //this makes it so that the final step is caught by the regex
+    var matches = recipeStepsString.match(/[1-9]?[0-9]\.\s(.+?\s+?)+?(?=(?!<pre[^>]*?>)[1-9]?[0-9]\.(?![^<]*?<\/pre>))/gm);
+    var output = "";
+    for(var i=0; i<matches.length; i++){
+        matches[i] = matches[i].replace(/[1-9]?[0-9]\.\s+/gm, '');
+        output+='<li>' + matches[i] + '</li>';
     }
-    
-    //alert("output: \n" + output);
-    return output;
-    //recipeStepsString.slice(i).search(/re/);
-    
-}
-
-function formatRecipeSteps2(recipeStepsString){
-    alert(recipeStepsString);
-    alert(recipeStepsString.search());
-    // .    /^[1-9][0-9]?$/m
+    return output
 }
